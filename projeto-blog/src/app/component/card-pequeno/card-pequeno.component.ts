0ebd1496d829e9a3133de9f3bcf869620a1a61cf
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-10-07 21:34:59 -0300

    att estilo e as propriedades do cardPequeno
    deixando preenchido os parametros para caso o usuario não queira passa-lo no html da pagina.
    
    E também deixando o arquivo html mais organizado

--- projeto-blog/src/app/component/card-pequeno/card-pequeno.component.ts
+++ projeto-blog/src/app/component/card-pequeno/card-pequeno.component.ts
@@ -6,10 +6,12 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card-pequeno.component.css']
 })
 export class CardPequenoComponent {
-  @Input() textoParagrafo: string = "";
+  @Input() imagemBanner: string = "../../../assets/img/img-retangular.png";
 
-  @Input() dataPostMateria: string = "";
+  @Input() dataPostMateria: string = "Outubro, 2023";
 
-  @Input() imagemBanner: string = "";
+  @Input() textoParagrafo: string = "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi excepturi alias rerum nostrum cumque necessitatibus placeat";
+
+
   
 }
