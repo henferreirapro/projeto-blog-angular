781be696ea4e1aab254f91ed90ab4cc14a910dd1
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-10-10 21:22:23 -0300

    Squashed commit of the following:
    commit afef3aee3675bde64c18df00702e2a3cfbd5456b
    Author: Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
    Date:   Tue Oct 10 21:20:32 2023 -0300
    
        Update app-routing.module.ts
    
    commit a5623c3c01f7909e17ab177807a655590c69ca0e
    Author: Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
    Date:   Tue Oct 10 21:18:41 2023 -0300
    
        Create dadosNoticias.ts

---
+++ projeto-blog/src/app/data/dadosNoticias.ts
@@ -0,0 +1,17 @@
+// Padrão dos Dados
+  // "id": 1,
+  // "imgNoticia": "",
+  // "dataPost": "",
+  // "tituloNoticia": "",
+  // "textoNoticia": "",
+
+
+export const bancoDadosNoticias = [
+  {
+    "id": 1,
+    "imgNoticia": "https://sm.ign.com/ign_br/screenshot/default/spider-man-2002-banner_k5qn.jpg",
+    "dataPost": "Setembro, 2023",
+    "tituloNoticia": "Novo Filme do Miranha",
+    "textoNoticia": "Saiu teaser para o novo filme do nosso miranha Tom Rola. \n O filme passará em uma nova cidade e com novos desafios.",
+  }
+]
