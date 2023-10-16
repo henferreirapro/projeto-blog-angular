b890f2ee623a939459d9f8ae2f8c192141f890fa
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2023-10-15 21:16:04 -0300

    Squashed commit: recebendo att dos componentes da page home

---
+++ projeto-blog/src/app/data/dadosNoticias.ts
@@ -0,0 +1,60 @@
+// Padrão dos Dados
+  // "id": "1",
+  // "imgConteudo": "",
+  // "dataPostConteudo": "",
+  // "tituloConteudo": "",
+  // "textoConteudo": ``,
+
+
+export const bancoDadosNoticias = [
+  { // Noticia 1
+    "id": "1",
+    "imgConteudo": "https://ovicio.com.br/wp-content/uploads/2023/06/20230625-untitled-730x365.jpg",
+
+    "dataPostConteudo": "Setembro, 2023",
+
+    "tituloConteudo": "Vigilante: Boku no Hero Academia Illegals ganhará anime, diz vazamento",
+
+    "textoConteudo": `De acordo com um vazamento circulando nas redes sociais, Vigilante: Boku no Hero Academia Illegals, também conhecido como My Hero Academia: Vigilantes, receberá uma adaptação em anime em breve.
+    Não dá detalhes adicionais, mas tudo indica que a produção ficará sob responsabilidade do estúdio Bones, assim como a série principal.
+    "Koichi Haimawari é um jovem universitário de bom coração que sonha ser um grande herói como All Might. Porém, por não ter autorização do governo como os defensores da justiça oficiais, ele passa a usar sua Individualidade às escondidas para ajudar os fracos e oprimidos nas ruas da cidade de Naruhata."
+
+    `,
+  },
+  { // Noticia 2
+    "id": "2",
+    "imgConteudo": "https://ovicio.com.br/wp-content/uploads/2023/10/20231011-jujutsu-kaisen-e36-nanami-730x365.jpg",
+
+    "dataPostConteudo": "12 de outubro de 2023",
+
+    "tituloConteudo": "Jujutsu Kaisen ganha prévia do Episódio 37",
+
+    "textoConteudo": `A segunda temporada retratou a história dos dias de Satoru Gojo e Suguru Geto como estudantes da Jujutsu Tech nos arcos O Inventário Oculto e Morte Prematura, trazendo detalhes de como os dois se separaram, algo apenas sugerido no longa-metragem Jujutsu Kaisen 0.
+    Em 31 de outubro, uma cortina de repente cai ao redor da Estação Shibuya, uma área repleta de pessoas celebrando o Halloween, e um grande número de civis ficam presos. Gojo se aventura em Shibuya sozinho, mas é uma armadilha preparada por Geto, Mahito, e outros espíritos amaldiçoados… Yuji Itadori, Megumi Fushiguro e Nobara Kugisaki se reúnem no local, onde uma batalha sem precedentes está prestes a começar.`,
+  },
+  { // Noticia 3
+    "id": "3",
+    "imgConteudo": "https://ovicio.com.br/wp-content/uploads/2022/09/20220912-ovicio-assassins-creed-mirage-duration-730x365.jpg",
+
+    "dataPostConteudo": "04 de outubro de 2023",
+
+    "tituloConteudo": "Review | Assassin’s Creed Mirage é (quase) uma volta às origens",
+
+    "textoConteudo": `Em 2017, a Ubisoft repaginou a sua franquia de maior sucesso, Assassin’s Creed, que vinha passando por um processo de saturação com lançamentos que já não traziam mais qualquer originalidade e que vinham amargando notas e vendas cada vez menores. Assassin’s Creed Origins foi a resposta a esta situação, e não poderia ser mais bem sucedido. Vendendo mais do que o dobro do capítulo anterior, Origins revitalizou a franquia, apresentou um gameplay mais robusto em uma nova engine, e inseriu os elementos de RPG que pautariam a série principal dali em diante.
+    Na sequência, vieram Odyssey e Valhalla, que seguiram as tendências de Origins e pesaram a mão no RPG - e não apenas isso, mas usaram e abusaram também da fantasia. Essa soma de fatores fez com que muita gente (especialmente jogadores mais antigos) começassem a reclamar do distanciamento da série de suas raízes; afinal, para se ter uma ideia, os protagonistas dos dois últimos games sequer eram assassinos (ou ocultos, como são chamados nesse período). Assim, quando uma das DLC’s planejadas para Assassin’s Creed Valhalla começou a tomar uma proporção grande demais, a Ubisoft decidiu que seria bom não apenas transformá-la em um jogo completo, mas também (por que não?) trazer uma espécie de “volta às origens”.
+    Dessa forma, surgiu Assassin’s Creed Mirage, um retorno ao clássico não apenas em escopo (com uma duração bem menor que a dos últimos jogos) mas também em tom e narrativa. Dessa vez, voltamos à pele de um Oculto/Assassino, Basim, um dos personagens vistos em Valhalla. Visitamos o passado desse personagem, seguindo a sua jornada de um batedor de carteiras em Bagdá para um membro da Ordem dos Ocultos. Mas será que essa promessa de “retorno aos clássicos” pode ser levada ao pé da letra? Seria esse um jogo para agradar apenas ao jogadores mais antigos? Ou seria o contrário, apenas mais um jogo de RPG com uma skin de Assassin’s Creed raiz? Bem, digamos que Mirage é um pouco de cada.`,
+  },
+  { // Noticia 4
+    "id": "4",
+    "imgConteudo": "https://ovicio.com.br/wp-content/uploads/2021/12/20211204-alabasta-ovicio.jpg",
+
+    "dataPostConteudo": "18 de setembro de 2023",
+
+    "tituloConteudo": "Por onde começar o anime e mangá de One Piece após a série",
+    
+    "textoConteudo": `Se você assistiu a série de One Piece e não quer começar do zero, mas seguir os eventos de onde a série parou, temos aqui um pequeno guia para te ajudar. Porém, antes de mais nada, devo dizer que a série live-action resume muitos eventos e faz algumas mudanças. Logo, o mais recomendável seria realmente recomeçar do zero, mas quem sou eu para julgar, certo?
+    Dito isto, caso você queira continuar os eventos a partir do anime, comece exatamente no episódio 62, que está disponível na Netflix e no Crunchyroll. Afinal, o mini-arco de Loguetown não está na série e você pode conferir o bando do Chapéu de Palha entrando na Grand Line e descobrindo mais sobre as peculiaridades deste lado do mar.
+    Já se você quer continuar lendo o mangá, você deve começar a ler a partir do capítulo 101 ou o volume 12 (vale dizer que a Panini está lançando a versão 3 em 1, então seria a partir do volume 4).
+    `,
+  },
+]
--- projeto-blog/src/app/pages/conteudo/conteudo.component.ts
+++ projeto-blog/src/app/pages/conteudo/conteudo.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { bancoDadosNoticias } from 'src/app/data/dadosNoticias';
 
 @Component({
   selector: 'app-conteudo',
   templateUrl: './conteudo.component.html',
   styleUrls: ['./conteudo.component.css']
 })
-export class ConteudoComponent {
+export class ConteudoComponent implements OnInit {
 
+  
+  @Input() imagemBanner: string = "";
+
+  @Input() dataPostMateria: string = "";
+
+  @Input() tituloCard: string = "";
+
+  @Input() textoParagrafo: string = "";
+
+  private id: string | null = "0"
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  // buscando o id do nosso componente card
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      value => this.id = (value.get("id"))
+    )
+
+    // enviando os valores do id para a nossa função
+    this.setValoresNoComponent(this.id)
+  }
+
+  // Pegandos os valores dos componentes no nosso banco de dados
+  setValoresNoComponent(id: string | null) {
+    const resultado = bancoDadosNoticias.filter( 
+      dados => dados.id == id
+    )[0]
+    // console.log(resultado)
+
+
+    this.imagemBanner = resultado.imgConteudo;
+    this.dataPostMateria = resultado.dataPostConteudo;
+    this.tituloCard = resultado.tituloConteudo;
+    this.textoParagrafo = resultado.textoConteudo;
+  }
 }
